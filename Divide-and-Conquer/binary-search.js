f6a9efde286190f9f42c43a1f72a29e5bfda2190
Aarushi <aarushisharda@gmail.com>
2018-06-11 22:56:28 +0530

    linear and binary search

---
+++ Divide-and-Conquer/binary-search.js
@@ -0,0 +1,20 @@
+// TASK: Implement binary search.
+
+function binarySearch(arr, target) {
+    let left = 0;
+      let right = arr.length - 1;
+      while (left <= right) {
+          const mid = left + Math.floor((right - left) / 2);
+          if (arr[mid] === target) {
+              return mid;
+          }
+          if (arr[mid] < target) {
+              left = mid + 1;
+          } else {
+              right = mid - 1;
+          }
+      }
+      return -1;
+  }
+  
+  binarySearch([2,6,7,90,103], 103);
