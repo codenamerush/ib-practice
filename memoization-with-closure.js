2aa32662791f9c8f9c748a5fa995cb823042c372
Aarushi <aarushisharda@gmail.com>
2018-06-11 19:49:09 +0530

    making it more reusable

--- memoization-with-closure.js
+++ memoization-with-closure.js
@@ -1,6 +1,8 @@
 const times10 = (n) => (n * 10);
+// Task 4: Make your memo function generic and accept the times10 function as a callback rather than defining the n * 10 logic inside the if/else or pulling it in from the global scope.
 
-const memoizedClosure = () => {
+// protip: Take advantage of the fact that parameters are saved in the closure as well, just like the cache from the previous example.
+const memoize = (cb) => {
   let cache = {};
 	return (n) => {
 		if (n in cache) {
@@ -9,15 +11,20 @@ const memoizedClosure = () => {
 		}
 		else {
 			console.log('Calculating result');
-			let result = times10(n);
+			let result = cb(n);
 			cache[n] = result;
 			return result;
 		}
 	};
-};
+
+}
 
 // returned function from memoizedAdd
-const memoClosureTimes10 = memoizedClosure();
-console.log('~~~~~~~~~~~~~~TASK 3~~~~~~~~~~~~~~');
-console.log('Task 3 calculated value:', memoClosureTimes10(9));	// calculated
-console.log('Task 3 cached value:', memoClosureTimes10(9));	// cached
+const memoizedTimes10 = memoize(times10);
+console.log('~~~~~~~~~~~~~~TASK 4~~~~~~~~~~~~~~');
+try {
+  console.log('Task 4 calculated value:', memoizedTimes10(9));	// calculated
+  console.log('Task 4 cached value:', memoizedTimes10(9));	// cached
+} catch(e) {
+  console.error('Task 4:', e)
+}
